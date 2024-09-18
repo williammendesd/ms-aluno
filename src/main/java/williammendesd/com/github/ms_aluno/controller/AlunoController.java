6bd080d8dfc421961421bc9ba9a1fdd9188e1397
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 09:41:51 -0300

    com CRUD

--- src/main/java/williammendesd/com/github/ms_aluno/controller/AlunoController.java
+++ src/main/java/williammendesd/com/github/ms_aluno/controller/AlunoController.java
@@ -1,15 +1,18 @@
 package williammendesd.com.github.ms_aluno.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import williammendesd.com.github.ms_aluno.DTO.AlunoDTO;
 import williammendesd.com.github.ms_aluno.model.Aluno;
 import williammendesd.com.github.ms_aluno.services.AlunoService;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -28,6 +31,29 @@ public class AlunoController {
     public ResponseEntity<AlunoDTO> findById(@PathVariable Integer id){
         AlunoDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoDTO> insert(@RequestBody @Valid AlunoDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<AlunoDTO> update(@PathVariable @NotNull Integer id,
+                                           @RequestBody @Valid AlunoDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
 INSERT INTO tb_aluno(nome, email, password, rm, status, turma) VALUES ('Ana Carolina', 'ana@hotmail.com', '654321', '98500', 'MATRICULADO','1STJ');
+
+INSERT INTO tb_aluno(nome, email, password, rm, status, turma) VALUES ('Roberto Miranda', 'roberto@hotmail.com', '654321', '98500', 'CANCELADO','2OPR');
+
+INSERT INTO tb_aluno(nome, email, password, rm, status, turma) VALUES ('Cezar Xandre', 'cezar@hotmail.com', '12346', '95005', 'MATRICULADO','1STF');
